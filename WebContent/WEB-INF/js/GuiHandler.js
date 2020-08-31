f88204f2d8dc5d65906fc7fda7b5b371f79074d3
h577870 <kristoffer.davidsen@gmail.com>
2020-08-31 17:44:52 +0200

    added showtask, removetask and other fun stuff

--- WebContent/WEB-INF/js/GuiHandler.js
+++ WebContent/WEB-INF/js/GuiHandler.js
@@ -1,29 +1,68 @@
 "use strict"
 
+import { Task } from './Task.js'
+
+let taskContainer = document.getElementsByClassName('taskcontainer')[0]
+
 class GuiHandler {
-	
+
 	/*
 	*Properties 'allStatuses', 'deleteTaskCallback' og 'newStatusCallback'
 	*/
-	
-	constructor(element) {
-		TODO
-	}
-	
+
 	//Prefix '_' i funksjonsnavn er for å markere at funksjonene er private.
 
 	_showTask(task) {
-		
-		TODO
+		const tbl = taskContainer.getElementsByTagName('table')[0]
+
+		const title = task._title
+		const status = task._status
+		const selector = document.createElement('select')
+		const option_w = document.createElement('option')
+		option_w.text = "hello"
+		selector.add(option_w)
+		const button = document.createElement('button')
+		button.textContent = "knapp"
+
+		const row = tbl.insertRow()
+		row.dataset.row_id = task._id
+		const td1 = row.insertCell(0)
+		const td2 = row.insertCell(1)
+		const td3 = row.insertCell(2)
+		const td4 = row.insertCell(3)
+
+		td1.appendChild(document.createTextNode(title))
+		td2.appendChild(document.createTextNode(status))
+		td3.appendChild(selector)
+		td4.appendChild(button)
+
+		button.addEventListener('click', async (e) => {
+			this._removeTask(row.dataset.row_id)
+			console.log(row.dataset.row_id)
+		})
+
+
 	}
 	_update(task) {
-		TODO
+		//TODO
 	}
 	_removeTask(id) {
-		TODO
+		let child = document.querySelector(`[data-row_id="${id}"]`)
+		child.parentNode.removeChild(child)
 	}
 	_noTask() {
-		TODO
+		//TODO
 	}
-	
+
 }
+const task_list = [
+	new Task(1, "Hello", 'ACTIVE'),
+	new Task(2, 'Hade', 'WAITING'),
+	new Task(3, 'Had', 'WAITING'),
+	new Task(4, 'ade', 'WAITING'),
+	new Task(5, 'Hde', 'WAITING')
+]
+const gui = new GuiHandler()
+task_list.forEach(task => {
+	gui._showTask(task)
+})
--- WebContent/WEB-INF/js/Task.js
+++ WebContent/WEB-INF/js/Task.js
@@ -1,32 +1,25 @@
 "use strict"
 
-class Task {
-	
+export class Task {
+
 	//Er det sånn man lager klasser? :P
-	
+
 	constructor(id, title, status) {
-		let task_id = id
-		let task_title = title
-		let task_status = status
-		
-		this.getTaskId = () => { return task_id }
-		this.getTaskTitle = () => { return task_title }
-		this.getTaskStatus = () => { return task_status }
-	
-		this.setTaskTitle = (title) => { task_title = title }
-		this.setTaskStatus = (status) => { task_status = status }
+		this._id = id
+		this._title = title
+		this._status = status
 	}
-	
+
 	get taskId() { return this.getTaskId }
 	get taskTitle() { return this.getTaskTitle }
 	get taskStatus() { return this.getTaskStatus }
-	
+
 	//Eclipse sutrer her...
 	/**
      * @param {any} new_title
      */
 	set taskTitle(new_title) { this.setTaskTitle(new_title) }
-	
+
 	//Her også...
 	/**
      * @param {any} new_status
