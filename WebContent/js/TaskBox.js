295c2e959e9591736de7599a17d8442278094882
Sølve Haugen <solvehaugen@158-37-234-66.hvl.no>
2020-09-01 16:10:40 +0200

    added TaskBox.js, style.css and updated index.html

--- WebContent/js/TaskBox.js
+++ WebContent/js/TaskBox.js
@@ -1,21 +1,45 @@
 "use strict"
 
-const modal = document.getElementById("modal")
+import {GuiHandler} from './GuiHandler.js'
+import {Task} from './Task.js'
 
-const button = document.getElementById("newTask")
-
+// Henter modal
+const modal = document.querySelector(`[data-modalID="myModal"]`)
+// Henter kanppen som opner modal
+const button = document.querySelector(`[data-buttonID="newTask"]`)
+// Henter span elementet som lukker modalen
 const span = document.getElementsByClassName("close")[0]
 
-button.onclick = function() {
-    modal.style.display = "none"
-}
+const title = document.querySelector(`[data-titleID = "title"]`)
+const status = document.querySelector(`[data-statusID ="status"]`)
 
-span.onclick = function (){
-    modal.style.display = "none"
-}
 
-window.onclick = function(event){
+//Henter submitknappen
+const submit = document.querySelector(`[data-submitID ="submit"]`)
+
+// Opner modalen når man trykker på knappen
+button.addEventListener('click', async() => {
+    modal.style.display = "block" 
+})
+
+//Lukker modalen når man trykker på (x)
+span.addEventListener('click', async() => {
+    modal.style.display = "none"
+})
+
+// Når man trykker utenfor modalen, lukker vinduet seg
+window.addEventListener('click', async(event) => {
     if (event.target == modal){
         modal.style.display = "none"
     }
-}
+})
+
+//Legge til task i tabellen (ikke ferdig)
+const gui = new GuiHandler()
+
+const task = new Task(6, title.value, status.value)
+submit.addEventListener('click', async() => {
+    gui._showTask(task)
+})
+
+
